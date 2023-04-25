67d59e4bda9a620eeb0d2e84cb6c0edf36f9c165
Anonymouss <mauludindayat@gmail.com>
2023-04-26 06:26:42 +0700

    newest

--- controller/UserController.js
+++ controller/UserController.js
@@ -83,6 +83,7 @@ export const Login = async (req, res) => {
     );
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({ accessToken });
