9d5e6e652fb3abb89acba03661d9bf19583a9abf
Anonymouss <mauludindayat@gmail.com>
2023-04-26 06:00:48 +0700

    newest

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://hidayatmauluddinportfolio.vercel.app"] }));
 app.use(express.json());
 app.use(FileUpload());
 app.use(express.static("public"));
