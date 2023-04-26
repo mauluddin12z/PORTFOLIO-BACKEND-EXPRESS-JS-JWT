459a40e3d6f087e5e436df283bfbadc7a49d239b
Anonymouss <mauludindayat@gmail.com>
2023-04-26 14:35:32 +0700

    newest

--- index.js
+++ index.js
@@ -7,19 +7,36 @@ import SkillRoute from "./routes/SkillRoute.js";
 import UserRoute from "./routes/UserRoute.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path"
+import path from "path";
 
 dotenv.config();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://hidayatmauluddinportfolio.vercel.app"] }));
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "https://hidayatmauluddinportfolio.vercel.app",
+    ],
+  })
+);
 app.use(express.json());
 app.use(FileUpload());
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-app.use('/uploads/certificates', express.static(path.join(__dirname, 'uploads/certificates')));
-app.use('/uploads/projects', express.static(path.join(__dirname, 'uploads/projects')));
-app.use('/uploads/skills', express.static(path.join(__dirname, 'uploads/skills')));
+app.use(
+  "/uploads/certificates",
+  express.static(path.join(__dirname, "uploads/certificates"))
+);
+app.use(
+  "/uploads/projects",
+  express.static(path.join(__dirname, "uploads/projects"))
+);
+app.use(
+  "/uploads/skills",
+  express.static(path.join(__dirname, "uploads/skills"))
+);
 app.use(ProductRoute);
 app.use(CertificateRoute);
 app.use(SkillRoute);
